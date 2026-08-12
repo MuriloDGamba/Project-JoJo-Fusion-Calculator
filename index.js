f655cf18a1d604551ffb154a05ce3ca7f8f8d4bc
Murilo Duarte Gâmba <murilo.dgamba@gmail.com>
2026-08-12 18:51:46 -0300

    Fixes and additions on the json's and creation of renderChoices function, alongside addition of select

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ const blessings = await bResponse.json()
 const aResponse = await fetch('./abilities.json')
 const abilities = await aResponse.json()
 
-console.log(traits)
-console.log(blessings)
-console.log(abilities)
+const inputAbility = document.querySelector('#inputAbility')
+const inputSub = document.querySelector('#inputSub')
+const inputBlessing = document.querySelector('#inputBlessing')
+const inputTrait = document.querySelector('#inputTrait')
+
+function renderChoice(array, container){
+    array.forEach(element => {
+        const option = document.createElement('option')
+        option.innerHTML = `${element.name}`
+
+        container.append(option)
+    });
+}
+
+function renderChoices(){
+    renderChoice(abilities, inputAbility)
+    renderChoice(abilities, inputSub)
+    renderChoice(blessings, inputBlessing)
+    renderChoice(traits, inputTrait)
+}
+
+function renderFusion(ability, subAbility, blessing, trait){
+    
+}
+
+renderChoices()
