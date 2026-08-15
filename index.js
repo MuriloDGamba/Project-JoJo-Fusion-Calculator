ad56ce5cb9d1ba253d2c4935c6d9f50a93afe1fd
Murilo Duarte Gâmba <murilo.dgamba@gmail.com>
2026-08-14 21:54:21 -0300

    Added new .json for sub-abilities, basic rendering just for function testing and some other adjustments

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const tResponse = await fetch('./traits.json')
-const traits = await tResponse.json()
+const aResponse = await fetch('./abilities.json')
+const abilities = await aResponse.json()
+
+const sResponse = await fetch('./subAbilities.json')
+const subAbilities = await sResponse.json()
 
 const bResponse = await fetch('./blessings.json')
 const blessings = await bResponse.json()
 
-const aResponse = await fetch('./abilities.json')
-const abilities = await aResponse.json()
+const tResponse = await fetch('./traits.json')
+const traits = await tResponse.json()
 
 const inputAbility = document.querySelector('#inputAbility')
 const inputSub = document.querySelector('#inputSub')
 const inputBlessing = document.querySelector('#inputBlessing')
 const inputTrait = document.querySelector('#inputTrait')
+
+const rendered = document.querySelector('#rendered')
 
 function renderChoice(array, container) {
     array.forEach(element => {
@@ -26,7 +31,7 @@ function renderSubChoices(array, container) {
         const option = document.createElement('option')
         option.innerHTML = `${element.name}`
 
-        if (element.fuseable) {
+        if (element.fuseable === true) {
             container.append(option)
         }
     });
@@ -34,13 +39,59 @@ function renderSubChoices(array, container) {
 
 function renderChoices() {
     renderChoice(abilities, inputAbility)
-    renderSubChoices(abilities, inputSub)
+    renderChoice(subAbilities, inputSub)
     renderChoice(blessings, inputBlessing)
     renderChoice(traits, inputTrait)
 }
 
 function renderFusion(ability, sub, blessing, trait) {
-    console.log(ability, sub, blessing, trait)
+    const abilityObj = abilities.find(obj => obj.name === ability)
+    const subObj = subAbilities.find(obj => obj.name === sub)
+    const blessingObj = blessings.find(obj => obj.name === blessing)
+    const traitObj = traits.find(obj => obj.name === trait)
+
+
+    const chosenAbility = document.createElement('div')
+    chosenAbility.id = 'ability'
+    chosenAbility.innerHTML = `${abilityObj.name}`
+
+    const chosenSub = document.createElement('div')
+    chosenSub.id = 'subAbility'
+    chosenSub.innerHTML = `${subObj.name}`
+
+    const chosenMoves = document.createElement('div')
+    chosenMoves.id = 'moves'
+    // Put here a call for a function that renders moves
+
+    const chosenBlessing = document.createElement('div')
+    chosenBlessing.id = 'blessing'
+    chosenBlessing.innerHTML = `
+        <div id="blessingName">${blessingObj.name}</div>
+        <div id="blessingDesc">${blessingObj.desc}</div>
+    `
+
+    const chosenTrait = document.createElement('table')
+    chosenTrait.id = 'trait'
+    chosenTrait.innerHTML = `
+        <tr>
+            <td colspan="2">${traitObj.name}</td>
+        </tr>
+        <tr>
+            <td>Potency</td>
+            <td>${traitObj.potency}</td>
+        </tr>
+        <tr>
+            <td>Speed</td>
+            <td>${traitObj.speed}</td>
+        </tr>
+        <tr>
+            <td>Resilience</td>
+            <td>${traitObj.resilience}</td>
+        </tr>
+    `
+
+
+    rendered.append(chosenAbility, chosenSub, chosenBlessing, chosenTrait, chosenMoves)
 }
 
 const fuse = document.querySelector('#fuse')
@@ -54,6 +105,8 @@ fuse.addEventListener('click', () => {
         showToast("You need to choose a blessing", "error")
     } else if (inputTrait.value === "") {
         showToast("You need to choose a trait", "error")
+    } else if (inputAbility.value == inputSub.value) {
+        showToast("Sub-ability cannot be the same as the main ability", "error")
     } else {
         showToast("Fusion created sucessfully!", "success")
         renderFusion(inputAbility.value, inputSub.value, inputBlessing.value, inputTrait.value)
